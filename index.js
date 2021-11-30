4a08b81949f6dd0f204e579efd2f23bf90282a3c
Ryan732 <rhorwitz732@gmail.com>
2021-11-29 21:00:34 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade").each(function() {
+      
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      
+      
+      if (objectBottom < windowBottom) { 
+        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      } else { 
+        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+      }
+    });
+  }).scroll(); 
+});
